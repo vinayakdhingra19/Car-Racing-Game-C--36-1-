22b19d19eb7e91a37689744f1d1f5e24a826b7f8
vinayakdhingra19 <60654040+vinayakdhingra19@users.noreply.github.com>
2020-04-27 17:55:51 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,54 @@
+class Game{
+    constructor(){
+
+    }
+    getState(){
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value",function(data){
+            gameState = data.val();
+        })
+    }
+    update(state){
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    async start(){
+        if(gameState === 0){
+            player = new Player();
+            var playerCountRef = await database.ref('playerCount').once("value");
+
+            if(playerCountRef.exists()){
+                playerCount = playerCountRef.val();
+                player.getCount();
+            }
+
+            form = new Form();
+            form.display();
+        }
+    }
+    play(){
+        form.hide();
+        text ("GAMESTART",120,100);
+        Player.getPlayerInfo();
+        if(allPlayers !== undefined){
+            var dp = 130;
+            
+            for(var plr in allPlayers){
+                if(plr === "player" + player.index){
+                    fill ("red");
+                }
+                else{
+                    fill("black");
+                }
+                dp = dp + 20;
+                text(allPlayers[plr].name + ":" + allPlayers[plr].distance,120,dp);
+            }
+        }
+        if(keyIsDown(UP_ARROW) && player.index !== null){
+            player.distance = player.distance + 20;
+            player.update();
+
+        }
+    }
+}
